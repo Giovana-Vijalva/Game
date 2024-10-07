aa384f3323dc14db986547bcc48cbe30cf001274
Giovana-Vijalva <161903742+Giovana-Vijalva@users.noreply.github.com>
2024-10-07 17:16:04 -0300

    Add files via upload

--- Exercicio2.cs
+++ Exercicio2.cs
@@ -19,5 +19,23 @@ class Exercicios2
         media = soma / nota4;
         Console.WriteLine(media);
 
+
+        int par1 = 2;
+        int par2 = 44;
+        int par3 = 32;
+        int par4 = 100;
+        int par5 = 286;
+
+        int impar1 = 599;
+        int impar2 = 53;
+        int impar3 = 687;
+        int impar4 = 333;
+        int impar = 11;
+
+
+        console
+
+
+
      }
 }
